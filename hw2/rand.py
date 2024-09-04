c79ab4811751eb4fe66393dab87b18353722e35c
Mili Jolly <66016518+MiliJolly@users.noreply.github.com>
2024-09-04 20:15:43 +0000

    Ran static analysis tools and saved post traces

--- hw2/rand.py
+++ hw2/rand.py
@@ -1,8 +1,7 @@
-import subprocess
+import random
+
 
 def random_array(arr):
-    shuffled_num = None
     for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
-        arr[i] = int(shuffled_num.stdout)
+        arr[i] = random.randint(1, 20)
     return arr

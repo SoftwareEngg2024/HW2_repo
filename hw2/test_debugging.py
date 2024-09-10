786826422e0b1e53c25bf5d5cfd23fea066dca79
rajkalantri <81515600+rajkalantri@users.noreply.github.com>
2024-09-10 00:27:49 +0000

    Ran static analysis tools and updated post-traces

--- hw2/test_debugging.py
+++ hw2/test_debugging.py
@@ -5,9 +5,11 @@ def test_A():
     arr = [0, 9, 8, 1, 2, 3, 4]
     assert mergeSort(arr) == [0, 1, 2, 3, 4, 8, 9]
 
+
 def test_B():
     arr = [5, 5, 5, 5, 5]
     assert mergeSort(arr) == [5, 5, 5, 5, 5]
+
 
 def test_C():
     arr = [None]
